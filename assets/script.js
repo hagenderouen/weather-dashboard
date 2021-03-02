c622f2c784233dbbd847efc2125997dd4b595610
Hagen DeRouen <hagend87@gmail.com>
2021-03-02 15:20:34 -0600

    Add starting assets.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+
+
+// TODO search form handler to get the city from user form input
+    // create weather api url
+    // fetch weather
+    // display city weather details
+    // display city 5 day forecast 
+// TODO search form submit event listener
